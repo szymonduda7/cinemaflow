5f78382bda16de77e588931055177be2711fd5a4
sdcautomations <sdcautomations@gmail.com>
2026-05-27 01:16:54 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import secrets from "../secret.json";
+
+const BASE_URL = "https://api.themoviedb.org/";
+
+function App() {
+  const [data, setData] = useState();
+
+  async function fetchData(url?: string) {
+    return await axios.get(`${BASE_URL}${url}?api_key=${secrets.apiKey}`, {});
+  }
+
+  useEffect(() => {
+    let data;
+
+    (async () => {
+      data = await fetchData("3/movie/11");
+      setData(data.data);
+    })();
+  }, []);
+
+  data && console.log(data);
+
+  return (
+    <div className="main-container">
+      <h1>Bibilioteka Filmów Web</h1>
+    </div>
+  );
+}
+
+export default App;
